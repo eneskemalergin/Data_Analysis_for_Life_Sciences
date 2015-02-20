6d0342dd1daee7b482c8889b2e3bacdf93ba09ab
eneskemalergin <eneskemalergin@gmail.com>
2015-02-20 14:36:15 -0600

    practice_Intro_random_Variables v0.2-

--- Statistics and R for Life Sciences/Week 2/practice_Intro_random_Variables.R
+++ Statistics and R for Life Sciences/Week 2/practice_Intro_random_Variables.R
@@ -1,4 +1,4 @@
-# Video Study from "Intro to Random Variables"
+# Video Study from "Intro to Random Variables 1-2, and null distributions"
 
 # Sets the working directory
 setwd("C:/Users/Pc-41/Desktop/Biomedical Data Science/Statistics and R for Life Sciences/Week 2")
@@ -6,9 +6,14 @@ setwd("C:/Users/Pc-41/Desktop/Biomedical Data Science/Statistics and R for Life 
 # Get's the data from specified file.
 data <- read.csv("femaleMiceWeights.csv")
 
-data[1:14,2]
+# Subsetting...
+data[1:12,2]
 
 data[13:24,2]
 
-mean(data[13:24,2])
-# [1] 26.83417
+# The change on the actual experiment and the control group...
+mean(data[13:24,2]) - mean(data[1:12,2])
+# [1] 3.020833
+
+# Now we will try to understand is this change caused by biological truths or just a thing that occured
+
