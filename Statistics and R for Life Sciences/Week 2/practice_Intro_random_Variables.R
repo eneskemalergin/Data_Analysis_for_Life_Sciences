807f9edb5dae0191b614706cba5ad9b51dbcb65a
eneskemalergin <eneskemalergin@gmail.com>
2015-02-22 14:07:02 -0600

    practice_Intro_random_Variables v0.3

--- Statistics and R for Life Sciences/Week 2/practice_Intro_random_Variables.R
+++ Statistics and R for Life Sciences/Week 2/practice_Intro_random_Variables.R
@@ -17,3 +17,21 @@ mean(data[13:24,2]) - mean(data[1:12,2])
 
 # Now we will try to understand is this change caused by biological truths or just a thing that occured
 
+population <- read.csv("femaleControlsPopulation.csv")
+
+control <- sample(population[,1],12)
+# Every time we will get different number.
+mean(control)
+
+# Make a 1000 different samples mean differences.
+n <- 1000
+null <- vector("numeric", 1000)
+for(i in 1:n){
+  control <- sample(population[,1],12)
+  treatment <- sample(population[,1],12)
+  null[i] < mean(treatment)-mean(control)
+}
+
+diff <- mean(data[13:24,2]) - mean(data[1:12,2])
+mean(null > diff)
+
