d6630429ce4324856c31e350380d4daa8f518517
eneskemalergin <eneskemalergin@gmail.com>
2015-02-22 14:45:36 -0600

    rafalib_installation added

---
+++ Statistics and R for Life Sciences/Week 2/rafalib_installation.R
@@ -0,0 +1,9 @@
+# rafalib package installation
+install.packages("devtools")
+install.packages("find_rtools")
+library(devtools)
+install_github("genomicsclass/dagdata")
+install_github("ririzarr/rafalib")
+
+library(RColorBrewer)
+library(rafalib)
