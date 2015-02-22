fe9c0770cefd670f8f997486fc6057f06dbaef34
eneskemalergin <eneskemalergin@gmail.com>
2015-02-22 14:30:44 -0600

    Completed Week 2's Video codes.

--- Statistics and R for Life Sciences/Week 2/practice_Intro_random_Variables.R
+++ Statistics and R for Life Sciences/Week 2/practice_Intro_random_Variables.R
@@ -35,3 +35,20 @@ for(i in 1:n){
 diff <- mean(data[13:24,2]) - mean(data[1:12,2])
 mean(null > diff)
 
+
+install.packages("rafalib")
+# Creating null distribution plot.
+n <- 100
+library(rafalib)
+mypar(1,1)
+plot(0,0,xlim = c(-5,5), ylim= c(1,30), type = "n")
+totals <- vector("numeric", 11)
+for(i in 1:n){
+  control <- sample(population[,1],12)
+  treatment <- sample(population[,1],12)
+  nulldiff <- mean(treatment) - mean(control)
+  j <- pmax(pmin(round(nulldiff)+6,11),1)
+  totals[j]<-totals[j]+1
+  text(j-6,totals[j],pch=15,round(nulldiff,1))
+  if(i < 15) scan()
+}
