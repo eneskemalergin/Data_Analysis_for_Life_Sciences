b8818815864799b3a1208dbd06396508c5901d5c
Enes Kemal Ergin <eneskemalergin@gmail.com>
2015-11-19 21:34:26 -0500

    .gitignore file added

--- Statistics and R for Life Sciences/Week 2/rafalib_installation.R
+++
@@ -1,9 +0,0 @@
-# rafalib package installation
-install.packages("devtools")
-install.packages("find_rtools")
-library(devtools)
-install_github("genomicsclass/dagdata")
-install_github("ririzarr/rafalib")
-
-library(RColorBrewer)
-library(rafalib)
